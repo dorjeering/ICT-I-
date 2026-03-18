52efc4d3da415b3f7610c8f17a6d23d816cc52e8
unknown <08250267.sce@rub.edu.bt>
2026-03-18 09:49:02 +0600

    first python demo code

--- Demo/stringEx.py
+++ Demo/stringEx.py
@@ -1,9 +1,12 @@
 print("Hello")
 print('Hello')
-multipleLines = '''This is a string that spans multiple lines.'''
+multipleLines = '''This is a string 
+that spans multiple lines.'''
+print(multipleLines)
 a=1
 b=2
 c=a + b
+print(c)
 print(type(c))
 c= str(c)
 print(type(c))
