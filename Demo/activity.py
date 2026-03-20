995b020ff84247588421c938750cf39bce24b4f1
unknown <08250267.sce@rub.edu.bt>
2026-03-20 16:14:21 +0600

    class activity

---
+++ Demo/activity.py
@@ -0,0 +1,12 @@
+n = input("Enter your name: ")
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+add_re = num1 + num2
+sub_re = num1 - num2
+mul_re = num1 * num2
+div_re = num1 / num2
+print("Adding your numbers:", add_re)
+print("Subtracting your numbers:", sub_re)
+print("Muktilplying your numbers:", mul_re)
+print("Dividing your numbers:", div_re)
+print("THANK YOU", n)
