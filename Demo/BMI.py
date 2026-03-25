7e3c93592dca1f023595ef1ecc85ba049449638b
unknown <08250267.sce@rub.edu.bt>
2026-03-25 08:50:31 +0600

    simple bmi calculator

---
+++ Demo/BMI.py
@@ -0,0 +1,10 @@
+H = float(input("Enter height(m): "))
+W = float(input("Enter weight: "))
+BMI = W / (H**2)
+print("Your BMI is:", BMI)
+if BMI <18.5:
+    print("You are underweight")
+elif 18.5<BMI<24.9:
+    print("You are normal")
+else:
+    print("You are overweight")
