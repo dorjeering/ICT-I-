26050f13389829e830fae402734fc69aa8a7390a
unknown <08250267.sce@rub.edu.bt>
2026-03-25 10:27:11 +0600

    string formatting 2

---
+++ Demo/str_format2.py
@@ -0,0 +1,4 @@
+print()
+print("We are in unit {}".format(2))
+print("{p} is a programming language. With the help of {p}, we can create various application. I love {c}".format(p = 'python', c = 'coding'))
+print()
