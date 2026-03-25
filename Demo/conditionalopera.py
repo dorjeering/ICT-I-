5c4feb97147a9718e569acb410ba01d8475fa1cd
unknown <08250267.sce@rub.edu.bt>
2026-03-25 09:54:45 +0600

    conditional operations

--- Demo/conditionalopera.py
+++ Demo/conditionalopera.py
@@ -2,7 +2,7 @@ m1 = float(input("Enter the marks of first subject: "))
 m2 = float(input("Enter the marks of second subject: "))
 m3 = float(input("Enter the marks of third subject: "))
 avg = (m1+m2+m3)/3
-print("Average: ",avg)
+print("Average:%.2f " %(avg))
 if (avg>=90 and m1>=50 and m2>=50 and m3>=50):
     print("Grade A")
 elif (avg>=80 and m1>=50 and m2>=50 and m3>=50):
