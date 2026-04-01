be1e29f2c2a1facf0230af888dac445d8504cbac
unknown <08250267.sce@rub.edu.bt>
2026-04-01 09:04:42 +0600

    adding nested if demo

---
+++ Demo/nestedIf.py
@@ -0,0 +1,12 @@
+print()
+age = int(input("Enter your age: "))
+if age>=18:
+    registered_voter = input("Are you a registered voter? (True/False): ")
+    registered_voter = registered_voter.lower() #convert input into boolean
+    if registered_voter == "true":
+        print("You are eligible to vote.")
+    else:
+        print("You need to register to vote.")
+else:
+    print("You are not eligible to vote.")
+print()
