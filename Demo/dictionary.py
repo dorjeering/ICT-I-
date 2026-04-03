f0d7bc2a475c4abbacf989a07625b512c520a793
unknown <08250267.sce@rub.edu.bt>
2026-04-03 15:52:04 +0600

    adding dictionary demo

---
+++ Demo/dictionary.py
@@ -0,0 +1,8 @@
+print()
+userDetails={'Id':1, 'userName':'Just_Me'} #Creating a dictionary with user details
+print(type(userDetails)) #Checking the type of the variable userDetails
+print(userDetails)
+location=dict(s='Samtse', t='Thimphu',p='Paro')
+print(type(location))#Checking the type of the variable location
+print(location)
+print()
