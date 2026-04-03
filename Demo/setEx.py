84349aa9c64dbd8021a4c80d2e46a85c22164d7c
unknown <08250267.sce@rub.edu.bt>
2026-04-03 15:30:20 +0600

    adding set demo

---
+++ Demo/setEx.py
@@ -0,0 +1,16 @@
+print()
+s = {1,2,3,"Hello",3.14,1,2,False} #creating a set
+print(type(s)) #checking the type of s
+print(s)
+s.add("World") #adding an element to a set
+print(s) #removing an element from a set
+ss={3,4,5}
+us=s.union(ss) #union of two sets
+print(us)
+ins=s.intersection(ss) #intersection of two sets
+print(ins)
+ds=s.difference(ss) #difference of two sets
+print(ds)
+s.clear() #clearing a set
+print(s) #deleting a set
+print()
