93236f64bbd2496198606e935f8711e0e8b6d147
unknown <08250267.sce@rub.edu.bt>
2026-04-03 16:14:12 +0600

    adding dictionary demo

--- Demo/dictionary.py
+++ Demo/dictionary.py
@@ -7,11 +7,18 @@ print(type(location))#Checking the type of the variable location
 print(location)
 print(location['s']) #Accessing the value of key 's' in the location dictionary
 print(location.get('t')) #Accessing the value of key 't' in the location dictionary using get() method
-print(location.get('d')) #Trying to access a non-existent key 'd' in the location dictionary using get() method with a default value
 location['b']='Bumthang' #Adding a new key-value pair to the location dictionary
 print(location)
 userDetails['email']='justme@example.com' #Adding a new key-value pair to the userDetails dictionary
 print(userDetails)
 userDetails['userName']='Just_Me_Updated' #Updating the value of the existing key 'userName' in the userDetails dictionary
 print(userDetails)
+del location['p'] #Deleting the key-value pair with key 'p' from the location dictionary
+print(location)
+dv=userDetails.pop('email') #Removing the key-value pair with key 'email' from the userDetails dictionary and storing the value in variable dv
+print(dv) 
+del_key, del_value=userDetails.popitem() #Removing the last inserted key-value pair from the userDetails dictionary and storing the key and value in variables del_key and del_value
+print(f'the deleted key is {del_key} and the deleted value is {del_value}')#Printing the deleted key and value
+location.clear() #Clearing all key-value pairs from the location dictionary
+print(location) #Printing the empty location dictionary
 print()
