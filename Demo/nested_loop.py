bb9e46e336785348f06561b367b845b86b20c87b
unknown <08250267.sce@rub.edu.bt>
2026-04-10 16:36:10 +0600

    adding nested loop demo

--- Demo/nested_loop.py
+++ Demo/nested_loop.py
@@ -13,8 +13,19 @@ for i in range(1,6):
         print(j, end=" ") 
     print()
 print("====="*24)
-for i in range(6,1,-1):
+for i in range(6,0,-1):
     for j in range(1,i):
-        print("$", end=" ") 
+        print("$", end="🤣  ") 
     print()
 print("====="*24)
+
+'''for i in range(1):
+    for j in range(1,i+3):
+        print("*", end=" ")'''
+for i in range(1):
+    for j in range(1,i+3):
+        print("*")
+for f in range(1):
+    for h in range(1,i+4):
+        print("*", end=" ")
+print()
