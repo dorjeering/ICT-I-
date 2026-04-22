14cc02efc32f7ac6dd515968feedf8bba7c97d63
unknown <08250267.sce@rub.edu.bt>
2026-04-22 09:46:22 +0600

    adding function demo

--- Demo/function.py
+++ Demo/function.py
@@ -10,3 +10,11 @@ def product():
 
 sum()#calling the sum function
 product()#calling the product function
+
+def sum_with_parameters(a, b):
+    print("The sum of ", a, "and ", b, "is: ", a + b)
+sum_with_parameters(3,7)
+
+def pwp(x,y):
+    print("The product of ",x ,"and ", y, "is: ",x*y)
+pwp(9,8)
