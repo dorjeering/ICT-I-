9d42a66608353afc2751ff627dd5e5dde78e7d3d
unknown <08250267.sce@rub.edu.bt>
2026-04-24 00:00:20 +0600

    adding even/odd function

---
+++ Demo/function_EvenOdd.py
@@ -0,0 +1,8 @@
+print()
+def even_odd(num):
+    if num %2 == 0:
+        print(num, " is an even number.")
+    else:
+        print(num, " is an odd number.")
+even_odd(int(input("Enter a number to check if it even or odd: ")))
+print()
