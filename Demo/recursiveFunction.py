c58279fc19e938b645c9f2f239bd536b1d8a8975
unknown <08250267.sce@rub.edu.bt>
2026-04-29 09:21:14 +0600

    adding recursive function demo

---
+++ Demo/recursiveFunction.py
@@ -0,0 +1,10 @@
+#sum of natural numbers using recursion
+print()
+def sum(n):
+    if n==1:
+        return 1
+    else:
+        return n + sum(n - 1)
+n = int(input("Enter a number: "))
+print("The sum of numbers from 1 to {} is: {}".format(n, sum(n)))
+print()
