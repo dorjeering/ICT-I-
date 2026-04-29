de3b596b108f3555a14e3de59e89ca7718ae7422
unknown <08250267.sce@rub.edu.bt>
2026-04-29 09:37:35 +0600

    adding recursive function demo

--- Demo/recursiveFunction.py
+++ Demo/recursiveFunction.py
@@ -8,3 +8,11 @@ def sum(n):
 n = int(input("Enter a number: "))
 print("The sum of numbers from 1 to {} is: {}".format(n, sum(n)))
 print()
+def fact(n):
+    if n==0:
+        return 1
+    else:
+        return n * fact(n - 1)
+n = int(input("Enter a number: "))
+print("The factorial of {} is: {}".format(n, fact(n)))
+print()
