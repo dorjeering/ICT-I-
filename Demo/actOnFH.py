30bab14829e1438723191028ca0f2498f034edb8
unknown <08250267.sce@rub.edu.bt>
2026-05-07 22:08:28 +0600

    adding student activity on file handler

--- Demo/actOnFH.py
+++ Demo/actOnFH.py
@@ -1,4 +1,4 @@
-file = open('Students.csv', 'w')
+file = open('Students.xlsx', 'w')
 file.write("Name, ID\n")
 file.write("Dorji, 001\n")
 file.write("Karma, 002\n")
@@ -6,13 +6,13 @@ file.write("Sonam, 003\n")
 file.write("Tashi, 004\n")
 file.write("Pema, 005\n")
 file.close()
-file = open('Students.csv', 'r')
+file = open('Students.xlsx', 'r')
 students = file.read()
 print(students)
 file.close()
 searchN = input("Enter a name to search: ")
 found = False
-with open('Students.csv', 'r') as file:
+with open('Students.xlsx', 'r') as file:
     for student in file:
         if searchN.lower() in student.lower():
             print(student)
